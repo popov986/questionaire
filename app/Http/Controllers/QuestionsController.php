e812242f762515543707e6d3c5205727c545de55
Vancho <popov_vancho@yahoo.com>
2019-05-04 23:07:21 +0200

    lesson form

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -34,7 +34,8 @@ class QuestionsController extends Controller
      */
     public function create()
     {
-        //
+        $question = new Question();
+        return view('questions.create',compact('question'));
     }
 
     /**
