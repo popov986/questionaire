d460b5017f8d23d3a53fda235471aa1d72b3b68a
Vancho <popov_vancho@yahoo.com>
2019-05-04 21:13:09 +0200

    styles

--- app/Question.php
+++ app/Question.php
@@ -25,6 +25,7 @@ class Question extends Model
         $this->attributes['slug'] = Str::slug($value);
     }
 
+    //Accessors
     public function getUrlAttribute()
     {
         return route('questions.show', $this->id);
@@ -33,6 +34,18 @@ class Question extends Model
     public function getCreatedDateAttribute()
     {
         return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute()
+    {
+        if($this->answers > 0 ){
+            if($this->best_answerd_id){
+                return 'answered-accepted';
+            }
+            return 'answered';
+        }
+
+        return 'unanswered';
     }
 
 }
