be9dfe184f446c8db575940280f11e57272f5dd4
Vancho <popov_vancho@yahoo.com>
2019-05-11 15:43:15 +0200

    question delete

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -96,6 +96,8 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
-        //
+        $question->delete();
+
+        return redirect('/questions')->with('success','Question deleted');
     }
 }
